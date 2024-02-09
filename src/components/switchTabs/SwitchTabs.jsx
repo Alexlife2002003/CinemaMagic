8a2c047069db703884540cd23373beb44c553b81
Alexia Hernandez Martinez <20200992@uaz.edu.mx>
2024-02-08 20:49:26 -0600

    switchtabs trending

---
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import "./style.scss";
+
+const SwitchTabs = ({ data, onTabChange }) => {
+    const [selectedTab, setSelectedTab] = useState(0);
+    const [left, setLeft] = useState(0);
+
+    const activeTab = (tab, index) => {
+        setLeft(index * 100)
+        setTimeout(() => {
+            setSelectedTab(index)
+        }, 300);
+        onTabChange(tab, index)
+    };
+
+    return (
+        <div className='switchingTabs'>
+            <div className="tabItems">
+                {data.map((tab, index) => (
+                    <span onClick={() => activeTab(tab, index)} key={index} className={`tabItem ${selectedTab === index ? "Active" : ""}`}>
+                        {tab}
+                    </span>
+                ))}
+                <span className="movingBg" style={{ left: left }} />
+            </div>
+        </div>
+    )
+};
+
+export default SwitchTabs
