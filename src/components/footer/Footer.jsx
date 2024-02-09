8caf88fe61eed58d9ebbbf5569e4d1147e91da65
Alexia Hernandez Martinez <20200992@uaz.edu.mx>
2024-02-08 19:56:29 -0600

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,48 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
 
-import "./style.scss"
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import img from "../../assets/tmdb.svg"
+
+import "./style.scss";
+
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          This product uses the TMDB API but is not endorsed or certified by TMDB.
+          <img className="img" src={img} alt="" />
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
