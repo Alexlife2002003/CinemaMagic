ab8689ecf2a55b55e06be982cc65000865bfe7b6
Alexia Hernandez Martinez <20200992@uaz.edu.mx>
2024-02-19 22:57:51 -0600

    details banner section

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,6 @@ import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
 const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_API_READ_ACCESS_TOKEN;
-const TMDB_TOKEN2 = import.meta.env.VITE_APP_TMDB_API_READ_ACCESS_TOKEN;
 const headers = {
     Authorization: "bearer "+TMDB_TOKEN,
 };
